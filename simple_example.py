1a4bd040a39378fff0ba213954f0cf9cda9bf29d
happyleaves <happyleaves.tfr@gmail.com>
2016-09-04 13:27:01 -0400

    add basic usage example

---
+++ simple_example.py
@@ -0,0 +1,20 @@
+import sys
+from mgz import header, body
+
+if __name__ == "__main__":
+    # For each input filename
+    for arg in sys.argv[1:]:
+        with open(arg, 'rb') as f:
+            # Remember end of file
+            f.seek(0, 2)
+            eof = f.tell()
+            f.seek(0, 0)
+            # Parse the header
+            h = header.parse_stream(f)
+            # Parse the body
+            while True:
+                # Parse a body operation
+                o = body.operation.parse_stream(f)
+                # Stop if all data has been read
+                if f.tell() == eof:
+                    break
