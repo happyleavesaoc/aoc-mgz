9e8b77116aed6581aa3dbdab3d29a17f408f30f4
Paulius <pauliusp@fastmail.com>
2021-06-18 12:46:03 +0300

    Added different parsing strings based on the size of the WALL event buffer (in .aoe2record files all x,y coords seem to be prefixed with a zer0)

--- mgz/fast/__init__.py
+++ mgz/fast/__init__.py
@@ -180,12 +180,15 @@ def parse_action(action_type, data):
         object_id, = struct.unpack_from('<3xI', data)
         return dict(object_ids=[object_id])
     if action_type == Action.WALL:
-        selection_count, player_id, x_start, y_start, x_end, y_end, z0, building_id, z1, const = struct.unpack_from(
-            '<7b2h1i', data)
-
-        object_ids = []
-        if selection_count > 0 and len(data) >= 15 + (selection_count * 4):
-            object_ids = struct.unpack_from('<' + str(selection_count) + 'I', data[15:])
+        offset = len(data) - data[0] * 4
+        if offset > 15:
+            # In DE recordings all coordinates are prefixed with a zero.
+            selection_count, player_id, x_start, y_start, x_end, y_end, building_id, z1, const = struct.unpack_from(
+                '<2bxbxbxbxbx2h1i', data)
+        else:
+            selection_count, player_id, x_start, y_start, x_end, y_end, building_id, z1, const = struct.unpack_from(
+                '<6bx2h1i', data)
+        object_ids = struct.unpack_from('<' + str(selection_count) + 'I', data[offset:])
 
         return dict(player_id=player_id,
                     x=x_start,
