49e0ebbeb2ba003997fdd27a2f9b2c481bc4137a
happyleavesaoc <happyleaves.tfr@gmail.com>
2022-11-14 22:43:27 +0000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mgz',
-    version='1.8.0',
+    version='1.8.1',
     description='Parse Age of Empires 2 recorded games.',
     url='https://github.com/happyleavesaoc/aoc-mgz/',
     license='MIT',
