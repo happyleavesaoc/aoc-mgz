fd07cb0d2179918a968ce02e9a9fdd29d45bc3d0
happyleavesaoc <happyleaves.tfr@gmail.com>
2025-08-17 14:13:32 +0000

    fix chat

--- mgz/common/chat.py
+++ mgz/common/chat.py
@@ -163,15 +163,16 @@ def parse_chat(line, encoding, timestamp, players, diplomacy_type=None, originat
     else:
         _parse_chat(data, line, players, diplomacy_type)
 
-    for age_marker in AGE_MARKERS:
-        if line.find(age_marker) > 0:
-            data['type'] = Chat.AGE
-            if any(marker in line for marker in FEUDAL_AGE_MARKERS):
-                data['age'] = Age.FEUDAL_AGE
-            if any(marker in line for marker in CASTLE_AGE_MARKERS):
-                data['age'] = Age.CASTLE_AGE
-            if any(marker in line for marker in IMPERIAL_AGE_MARKERS):
-                data['age'] = Age.IMPERIAL_AGE
+    if data['type'] is not Chat.DISCARD:
+        for age_marker in AGE_MARKERS:
+            if line.find(age_marker) > 0:
+                data['type'] = Chat.AGE
+                if any(marker in line for marker in FEUDAL_AGE_MARKERS):
+                    data['age'] = Age.FEUDAL_AGE
+                if any(marker in line for marker in CASTLE_AGE_MARKERS):
+                    data['age'] = Age.CASTLE_AGE
+                if any(marker in line for marker in IMPERIAL_AGE_MARKERS):
+                    data['age'] = Age.IMPERIAL_AGE
 
     if not _validate(data, players):
         data['type'] = Chat.DISCARD
