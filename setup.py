a68909ea5895e2381530a82f9a6af9d4d0657630
happyleavesaoc <happyleaves.tfr@gmail.com>
2026-01-02 17:26:14 +0000

    bump aocref

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mgz',
-    version='1.8.44',
+    version='1.8.45',
     description='Parse Age of Empires 2 recorded games.',
     url='https://github.com/happyleavesaoc/aoc-mgz/',
     license='MIT',
@@ -11,7 +11,7 @@ setup(
     author_email='happyleaves.tfr@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'aocref>=2.0.29',
+        'aocref>=2.0.33',
         'construct==2.8.16',
         'dataclasses==0.8; python_version < "3.7"',
         'tabulate>=0.9.0',
